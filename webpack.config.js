cf2e50f6a532ddfc0cec865e9c00687f18d5f476
schnetzi <schnetzi@users.noreply.github.com>
2020-11-26 23:52:17 +0100

    Add login page

--- webpack.config.js
+++ webpack.config.js
@@ -1,76 +1,81 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  mode: "development",
-  entry: {
-    index: "./src/index",
-    countdown: "./src/views/countdown/countdown",
-    article: "./src/views/article/article",
-  },
-  resolve: {
-    extensions: [".ts", ".tsx", ".js", ".json"],
-  },
-  devtool: "inline-source-map",
-  devServer: {
-    contentBase: "./dist",
-  },
-  plugins: [
-    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
-    new HtmlWebpackPlugin(), // index.html
-    new HtmlWebpackPlugin({
-      filename: "countdown.html",
-      template: "src/views/countdown/countdown.html",
-    }),
-    new HtmlWebpackPlugin({
-      filename: "article.html",
-      template: "src/views/article/article.html",
-    }),
-  ],
-  output: {
-    filename: "[name].[contenthash].js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  optimization: {
-    runtimeChunk: "single",
-    splitChunks: {
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name: "vendors",
-          chunks: "all",
-        },
-      },
-    },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|js)x?$/,
-        exclude: /node_modules/,
-        loader: "babel-loader",
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          "style-loader",
-          {
-            loader: "css-loader",
-            options: {
-              importLoaders: 1,
-            },
-          },
-          "postcss-loader",
-          {
-            loader: "sass-loader",
-            options: {
-              // Prefer `dart-sass`
-              implementation: require("sass"),
-            },
-          },
-        ],
-      },
-    ],
-  },
+	mode: 'development',
+	entry: {
+		index: './src/index',
+		countdown: './src/views/countdown/countdown',
+		article: './src/views/article/article',
+		login: './src/views/login/login',
+	},
+	resolve: {
+		extensions: ['.ts', '.tsx', '.js', '.json'],
+	},
+	devtool: 'inline-source-map',
+	devServer: {
+		contentBase: './dist',
+	},
+	plugins: [
+		new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+		new HtmlWebpackPlugin(), // index.html
+		new HtmlWebpackPlugin({
+			filename: 'countdown.html',
+			template: 'src/views/countdown/countdown.html',
+		}),
+		new HtmlWebpackPlugin({
+			filename: 'article.html',
+			template: 'src/views/article/article.html',
+		}),
+		new HtmlWebpackPlugin({
+			filename: 'login.html',
+			template: 'src/views/login/login.html',
+		}),
+	],
+	output: {
+		filename: '[name].[contenthash].js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+	optimization: {
+		runtimeChunk: 'single',
+		splitChunks: {
+			cacheGroups: {
+				vendor: {
+					test: /[\\/]node_modules[\\/]/,
+					name: 'vendors',
+					chunks: 'all',
+				},
+			},
+		},
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(ts|js)x?$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+			},
+			{
+				test: /\.s[ac]ss$/i,
+				use: [
+					'style-loader',
+					{
+						loader: 'css-loader',
+						options: {
+							importLoaders: 1,
+						},
+					},
+					'postcss-loader',
+					{
+						loader: 'sass-loader',
+						options: {
+							// Prefer `dart-sass`
+							implementation: require('sass'),
+						},
+					},
+				],
+			},
+		],
+	},
 };
